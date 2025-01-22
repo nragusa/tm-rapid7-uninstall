8c24548ed3e4678fd3090bc4b6849f4adfd66a8f
Nick Ragusa <nragusa@gmail.com>
2025-01-22 13:52:40 -0500

    updates

---
+++ README.md
@@ -0,0 +1,20 @@
+# Overview
+
+This script will accept a CSV file containing a list of EC2 instance IDs, check the validity of each ID, and then use AWS Systems Manager to remove a package that was previously installed with AWS Systems Manager Distributor.
+
+## Usage
+
+```bash
+python uninstall.py -p <package> <path to csv>
+```
+
+The script assumes a header in the file and that the first column contains the instance ID. For example:
+
+```csv
+resourceId, package
+i-12345678910, MyPackage
+i-21234567891, MyPackage
+...
+```
+
+You may also need to specify the region to use. At the top of the script, set `REGION_NAME` appropriately.
--- uninstall.py
+++ uninstall.py
@@ -4,14 +4,14 @@ import argparse
 import csv
 import logging
 import sys
+from typing import List
 import boto3
 from botocore.exceptions import ClientError
 
-PACKAGE_NAME = 'wget'
-REGION_NAME = 'us-east-2'
+# Configuration
+REGION_NAME = 'us-east-1'
 
-
-# Basic setup
+# Logging
 logging.basicConfig(
     level=logging.INFO,
     format='%(asctime)s - %(levelname)s - %(message)s',
@@ -19,7 +19,7 @@ logging.basicConfig(
 )
 
 
-def uninstall_package(items, batch_size=50):
+def uninstall_package(items: List[str], package: str, batch_size: int = 50) -> None:
     """
     Process items in batches of specified size.
 
@@ -30,10 +30,10 @@ def uninstall_package(items, batch_size=50):
     for i in range(0, len(items), batch_size):
         batch = items[i:i + batch_size]
         # Process your batch here
-        process_batch(batch)
+        process_batch(batch, package)
 
 
-def process_batch(batch):
+def process_batch(batch: List[str], package: str) -> None:
     """
     Runs the `AWS-ConfigureAWSPackage` AWS SSM Run Document against a batch of up to
     50 valid EC2 instance IDs
@@ -44,17 +44,17 @@ def process_batch(batch):
     client = boto3.client('ssm', region_name=REGION_NAME)
     try:
         logging.info('Uninstalling %s from batch: %s',
-                     PACKAGE_NAME, str(batch))
+                     package, str(batch))
         client.send_command(
             InstanceIds=batch,
             DocumentName='AWS-ConfigureAWSPackage',
             Parameters={
                 'action': ['Uninstall'],
-                'name': [PACKAGE_NAME]
+                'name': [package]
             },
             CloudWatchOutputConfig={
                 'CloudWatchOutputEnabled': True,
-                'CloudWatchLogGroupName': f'/tm/{PACKAGE_NAME.lower().replace(' ', '-')}Uninstall'
+                'CloudWatchLogGroupName': f'/tm/{package.lower().replace(' ', '-')}Uninstall'
             }
         )
     except ClientError as e:
@@ -62,7 +62,7 @@ def process_batch(batch):
         sys.exit(1)
 
 
-def check_instances(instance_ids):
+def check_instances(instance_ids: List[str]) -> List[str]:
     """
     Checks that the passed EC2 instance IDs are both valid (meaning they exist in the
     AWS account and region) and that the EC2 instance is currently running. If both are
@@ -116,14 +116,20 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         description='Uninstalls a package from EC2 instances that was previously installed with AWS Systems Manager Distributor')
     parser.add_argument(
-        'resource_id_file', help='The name of the CSV file containing the resource IDs', nargs='?')
+        'resource_id_file', help='The name of the CSV file containing the resource IDs')
+    parser.add_argument(
+        '-p', '--package-name', help='The name of the package to uninstall', required=True)
     args = parser.parse_args()
 
     if args.resource_id_file is None:
         parser.print_help()
         sys.exit(1)
+    if args.package_name is None:
+        parser.print_help()
+        sys.exit(1)
 
     resource_id_file = args.resource_id_file
+    package_name = args.package_name
 
     # Read the CSV file
     with open(resource_id_file, 'r', encoding='utf-8') as f:
@@ -136,4 +142,4 @@ if __name__ == '__main__':
     valid_instances = check_instances([item[0] for item in resource_id_list])
 
     # Uninstall package from valid instances
-    uninstall_package(valid_instances)
+    uninstall_package(valid_instances, package_name)
